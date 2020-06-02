bbc2eb98273b59a522be36f455ebbec27c02e9c6
MinThetMaung <minthetmaung96@gmail.com>
2020-06-02 15:22:19 +0630

    Initial Commit

---
+++ Shoppie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Shoppie
+//
+//  Created by Min Thet Maung on 02/06/2020.
+//  Copyright © 2020 Myanmy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
